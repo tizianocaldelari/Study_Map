497620e4b76e3e38be87e5dcdeeee1ed9114c088
314a <3.14a@bluewin.ch>
2023-04-17 22:45:20 +0200

    PostgreSQL Abfrage als FastAPI Endpoint hinzufügen

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import HTMLResponse
 from fastapi.responses import ORJSONResponse
 # CORS aktivieren für FastAPI Backend
 from fastapi.middleware.cors import CORSMiddleware
+# Datenbank Verbindung
+from psycopg2 import pool
+#from psycopg2.extras import RealDictCursor
+from pydantic import BaseModel
 
 app = FastAPI() 
 
@@ -105,5 +109,57 @@ async def read_points():
 # Post Query - test on the OPENAPI Docs Page
 @app.post("/square")
 def square(some_number: int) -> dict:
-    square = some_number * some_number
-    return {f"{some_number} squared is: ": square}
+	square = some_number**2
+	return {f"{some_number} squared is: ": square}
+
+
+# Simple Database query
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_NAME = "geoserver"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+DB_POOL_MIN_CONN = 1
+DB_POOL_MAX_CONN = 10
+
+db_pool = pool.SimpleConnectionPool(
+  DB_POOL_MIN_CONN, DB_POOL_MAX_CONN, host=DB_HOST, port=DB_PORT, database=DB_NAME, user=DB_USER, password=DB_PASSWORD
+)
+
+# Definition für das Antwortschema (response schema) für den Endpunkt getPoints
+class PunkteResponse(BaseModel):
+    id: int
+    name: str
+    x: float
+    y: float
+    geom: str
+    
+# Funktion für den getPoints-Endpunkt
+# Test: curl http://localhost:8000/getPoints   
+@app.get("/getPoints" , response_model=list[PunkteResponse])
+async def get_punkte():
+    conn = None
+    try:
+        # Verbindung zur Datenbank über den Verbindungspool herstellen
+        conn = db_pool.getconn()
+        cur = conn.cursor()
+        query = "SELECT id, name, ST_X(geom) as x, ST_Y(geom) as y, ST_AsText(geom) as geom FROM punkte"
+        cur.execute(query)
+        results = cur.fetchall()
+        # Ergebnisse in Pydantic-Modelle umwandeln und zurückgeben
+        punkte = []
+        for row in results:
+        # Prüfen, ob die Ergebnisse ausreichend Spalten enthalten
+             if len(row) > 4:
+                  punkte.append(PunkteResponse(id=row[0], name=row[1], x=row[2], y=row[3], geom=row[4]))   
+        #print(punkte)
+        return punkte
+    except Exception as e:
+        print(e)
+        # Eine HTTPException mit Statuscode 500 (Interner Serverfehler) auslösen und den ausgelösten Fehler als Detail übergeben
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error: "+str(e))
+    finally:
+        if conn:
+            # Die Verbindung zur Datenbank beenden
+            db_pool.putconn(conn)
+
