ae01bb12e74e03dc5951ff5225c3ff20cadf8b31
Stefan Eberlein <stefan.eberlein@fhnw.ch>
2024-03-26 10:14:34 +0100

    add OpenLayers x MUI interaction

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,28 @@
 import './App.css';
-
-// react
-import React, { useState, useEffect } from 'react';
-
-// openlayers
-import GeoJSON from 'ol/format/GeoJSON'
-//import Feature from 'ol/Feature';
-
-// components
-import MapWrapper from './components/MapWrapper';
+import React from 'react'
+import { useState } from 'react'
+import MapWrapper from './components/MapWrapper.jsx'
+import FeatureTable from './components/FeatureTable.jsx'
 
 function App() {
-  
-  // set intial state
-  const [ features, setFeatures ] = useState([])
-
-  // initialization - retrieve GeoJSON features from Mock JSON API get features from mock 
-  // GeoJson API (read from flat .json file in public directory)
-  useEffect(() => {
-    // Folgende URL nutzen, falls das Server Backend läuft (FastAPI)
-    // fetch('http://localhost:8000/points/')
-    // Test Geojson Datei, falls das Server Backend nicht läuft (FastAPI)
-    fetch('/geojson_points.json')
-      .then(response => response.json())
-      .then((fetchedFeatures) => {
-
-        // parse fetched geojson into OpenLayers features
-        // use options to convert feature from EPSG:4326 to EPSG:3857
-        const wktOptions = {
-          dataProjection: 'EPSG:4326',
-          featureProjection: 'EPSG:3857'
-        }
-        const parsedFeatures = new GeoJSON().readFeatures(fetchedFeatures, wktOptions)
-
-        // set features into state (which will be passed into OpenLayers
-        //  map component as props)
-        setFeatures(parsedFeatures)
-
-      })
-
-  },[])
-  // <MapWrapper features={features} />
+  const [features, setFeatures] = useState([]);
+  const [selectedFeatureID, setSelectedFeatureID] = useState();
   return (
-    <div className="App">      
-      <div className="app-label">
-        <p>GDI Projekt Template</p>        
-      </div>      
-      <MapWrapper features={features} />
-    </div>
+    <>
+      <h1>
+        <a href="https://openlayers.org/">
+          <img src='./OpenLayers_logo.svg' className="logo" alt="OpenLayers logo" />
+        </a>OpenLayers
+        &
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src='./react.svg' className="logo" alt="React logo" />
+        </a>React
+      </h1>
+      <div id='content'>
+        <MapWrapper features={features} setFeatures={setFeatures} selectedFeatureID={selectedFeatureID} setSelectedFeatureID={setSelectedFeatureID} />
+        <FeatureTable features={features} setFeatures={setFeatures} selectedFeatureID={selectedFeatureID} setSelectedFeatureID={setSelectedFeatureID} />
+      </div>
+    </>
   )
 }
 export default App
