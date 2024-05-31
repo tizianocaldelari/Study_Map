af90a0343115474301ffac7e82cafc96f347171b
tizianocaldelari <tiziano.caldelari@students.fhnw.ch>
2024-05-31 13:15:03 +0200

    deleted:    client/src/components/Header.css 	modified:   client/src/components/Header.jsx 	modified:   client/src/components/MapWrapper.css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './Header.css'; // Ensure you import the CSS file
 
 const Header = () => {
   return (
     <header className="header-container">
       <div className="header-content">
-        <h1 className="header-title">Study_Map</h1>
+        <h1 className="header-title">Study Map</h1>
       </div>
     </header>
   );
